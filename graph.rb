3f0b785f01bf2c0b0b3e2ae6de8dd321d5f96777
Angel <a1akaa2z@gmail.com>
2021-10-02 04:02:21 -0400

    initial set up for part 1 which is creating a simple graph with the basic functionality of adding nodes and edges and getting a list of edges as well as the string representation, which will be the adjacency lists of the graph

---
+++ graph.rb
@@ -0,0 +1,28 @@
+class Graph
+    attr_accessor :num_nodes, :num_edges
+
+    def initialize
+        @num_nodes = 0
+        @num_edges = 0
+
+    end
+
+    def add_node(s)
+    end
+
+    def add_nodes(list)
+    end
+
+    def add_edge(n1, n2)
+    end
+
+    def get_edges
+    end
+
+    def to_s
+        #string of adj lists
+        puts "Test"
+    end
+
+    
+end
